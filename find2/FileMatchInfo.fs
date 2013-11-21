41be271b05fea1de8a2996fb9a4ac1cab0d722f9
Aleksey Berezan <aleksey.berezan@gmail.com>
2013-11-21 00:23:36 -0800

    Added FileMatchInfo.fs

---
+++ find2/FileMatchInfo.fs
@@ -0,0 +1,22 @@
+﻿namespace find2
+open System
+open System.IO
+
+[<StructAttribute>]
+type internal FileMatchInfo = 
+    val public FileInfo:FileInfo  
+    val public Matched:bool
+    val public MatchedLines:seq<(int*string)>
+    val public ErrorOccurred:bool
+
+    private new (fileInfo:FileInfo, matchedLines:seq<(int*string)>, errorOccurred:bool) =
+        {
+            FileInfo = fileInfo;
+            ErrorOccurred = errorOccurred;
+            MatchedLines = matchedLines;
+            Matched = (Seq.isEmpty matchedLines);
+        }
+
+    static member NoMatches fileInfo = new FileMatchInfo(fileInfo, Seq.empty, false)
+    static member WithMatches fileInfo matchedLines = new FileMatchInfo(fileInfo, matchedLines, false)
+    static member WithError fileInfo = new FileMatchInfo(fileInfo, Seq.empty, true)
